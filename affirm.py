83979bd682bf389e251bfaf11db71be461c5ad39
hamnox <hamnox@users.noreply.github.com>
2015-11-19 05:22:47 -0800

    Add more random affirmations to affirm.py
    will need to change some of them so they're not obnoxious

--- affirm.py
+++ affirm.py
@@ -32,6 +32,11 @@ def substr_found(haystack):
     else:
         return True
 
+# http://www.secasa.com.au/pages/101-phrases-of-praise/
+# http://www.creativeaffirmations.com/anxiety.html
+# http://www.creativeaffirmations.com/daily-affirmations.html
+# http://www.creativeaffirmations.com/stress-management-affirmations.html
+
 affirmations = ["I love you",
     "You're great",
     "Great job",
@@ -128,7 +133,103 @@ affirmations = ["I love you",
     "You deserve a kiss",
     "You're such a smart kid",
     "You brighten my day",
-    "Super job"]
+    "Super job",
+    
+    
+    
+    "Take a time-out. Practice yoga, listen to music, meditate, get a massage, or learn relaxation techniques. Stepping back from the problem helps clear your head.",
+    "Eat well-balanced meals. Do not skip any meals. Do keep healthful, energy-boosting snacks on hand.",
+    "Limit alcohol and caffeine, which can aggravate anxiety and trigger panic attacks.",
+    "Get enough sleep. When stressed, your body needs additional sleep and rest.",
+    "Exercise daily to help you feel good and maintain your health.",
+    "Take deep breaths. Inhale and exhale slowly.",
+    "Count to 10 slowly. Repeat, and count to 20 if necessary.",
+    "Do your best. Instead of aiming for perfection, which isn't possible, be proud of however close you get.",
+    "Accept that you cannot control everything. Put your stress in perspective: Is it really as bad as you think?",
+    "Welcome humor. A good laugh goes a long way.",
+    "Maintain a positive attitude. Make an effort to replace negative thoughts with positive ones.",
+    "Get involved. Volunteer or find another way to be active in your community, which creates a support network and gives you a break from everyday stress.",
+    "Learn what triggers your anxiety. Is it work, family, school, or something else you can identify? Write in a journal when you're feeling stressed or anxious, and look for a pattern.",
+    "Talk to someone. Tell friends and family you're feeling overwhelmed, and let them know how they can help you. Talk to a physician or therapist for professional help.",
+    "Get help online. Lantern ( https://golantern.com/anxiety/?utm_source=adaa ) offers online programs guided by professional coaches to help you turn healthy anxiety management into a habit. (Sponsored)",
+
+    "5 X 30: Jog, walk, bike, or dance three to five times a week for 30 minutes.",
+    "Set small daily goals and aim for daily consistency rather than perfect workouts. It's better to walk every day for 15-20 minutes than to wait until the weekend for a three-hour fitness marathon. Lots of scientific data suggests that frequency is most important.",  
+    "Find forms of exercise that are fun or enjoyable. Extroverted people often like classes and group activities. People who are more introverted often prefer solo pursuits.",
+    "Distract yourself with an iPod or other portable media player to download audiobooks, podcasts, or music. Many people find it's more fun to exercise while listening to something they enjoy.",
+    "Recruit an \"exercise buddy.\" It's often easier to stick to your exercise routine when you have to stay committed to a friend, partner, or colleague.",
+    "Be patient when you start a new exercise program. Most sedentary people require about four to eight weeks to feel coordinated and sufficiently in shape so that exercise feels easier.",
+
+
+    "I release any anxiety I am holding onto.",
+    "My current state of being is peaceful.",
+    "I am calm.",
+    "Each and every day, my circumstances are improving.",
+    "Peace. Peace. Peace...",
+    "I embrace Divine balance in my life. Everything is as it should be.",
+    "I am consciously aware of the calm silence of my spirit.",
+    "My awareness is anchored in tranquility.",
+    "I am in the Divine flow of life.",
+    "I am stable during life's ups and downs.", 
+    "I am willing to let my anxious thoughts go. Each day I embrace a laid-back attitude.",
+    "I choose to breathe deeply.",
+    "From head to toe, I am relaxed.",
+    "I am centered.",
+
+    "Today, I will act out of love.",
+    "I choose to graciously extend kindness to everyone I meet.",
+    "I will do my absolute best.",
+    "I will make choices that demonstrate the care I have for my body.",
+    "I freely express gratitude for the many blessings I receive.",
+    "I will speak kindly to and of others.",
+    "I choose to see the good in the people I interact with today.",
+    "I will set aside time to be silent and breath.",
+    "I have the power to control my reactions to the challenges I will face.",
+    "I choose to see each obstacle as an opportunity to grow.",
+
+    "Breathe in; breathe out. Breathe in; breathe out. Breathe in; breathe out.",
+    "I choose to focus on the things I can control.",
+    "I place my attention on the next thing to do, and only the next thing to do.",
+    "I release the things that are not my responsibility to control.",
+    "I allow myself to take a break and do something I enjoy.",
+    "I am willing to delegate a portion of my responsibilities.",
+    "I am okay. All will be okay.",
+    "I choose to approach my problems with a calm heart and mind.",
+    "Things will be what they will be. I cannot change the past or completely control the future.",
+    "I relax into the present moment knowing that this is the only thing that needs my attention right now."
+
+    "Each day I take a small step toward achieving my goals.",
+    "I am willing to step outside of my comfort zone to accomplish the goals I set for myself.",
+    "The path toward my goal is clear.",
+    "I am willing to devote time and energy toward my goals. They are a priority in my life.",
+    "I honor my mission and values by pursuing my goals.",
+    "I can clearly see my goal already accomplished. ",
+    "I accept that there will be challenges when pursuing my goals. I have the knowledge and ability to overcome anything in my path.",
+    "The resources I need are becoming available to help me achieve my goals. ",
+    "My hard work is paying off already.",
+    "I only focus on the next step to take and trust that I am being lead toward the best and highest fulfillment of my goals.",
+    "Today is filled with opportunity. Everywhere I look I see it, and I trust my intuition to follow where it leads. ",
+    "Good Morning Life! I am so grateful to be alive today.",
+    "Today I will trust divine guidance to head my actions knowing that I will be brought to joy & prosperity.",
+    "Good Morning! Yes, it will be a good morning and a good day!",
+    "I am grateful for today.",
+    "The sun is shining through my window and through my heart.",
+    "I enter this day with an attitude of appreciation.",
+    "Today my world is changing for the better. I open my awareness to my shifting reality.",
+    "I allow my intuition to guide my actions to day with trust that I am guided toward my highest good.",
+    "Today I will be pleasantly surprised.",
+    "I enter today with an open mind and a calm presence.",
+    "I am proud of myself.",
+    "Positive energy flows through me; each cell of my being is awake and alive with joy.",
+    "I radiate love and joy to all I meet.",
+    "I am whole, complete and perfect just as I am, right where I am at. ",
+    "I am more than capable of bringing my dreams to life. ",
+    "I love me.",
+    "I choose to be on my side. All of my thoughts are pointed toward my positive intentions.",
+    "I release all negativity that is blocking the divine expression of who I am. ",
+    "I am okay. I am breathing. I am alive. I am experiencing this moment. I release all worry, all thoughts of past and future. I am here, now.",
+    "I am safe. There is Divine protection surrounding me.",
+]
 
 if (args.search != None):
     affirmations = filter(substr_found, affirmations)
